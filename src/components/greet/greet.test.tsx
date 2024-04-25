17b912c64df436635ff0a6be3cb6ffae7c88e366
H-G-Kim <heegenek@gmail.com>
2024-04-25 21:23:53 +1000

    na

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,12 @@
+import {render} from '@testing-library/react'
+import { Greet } from './greet'
+
+
+//test()
+
+test('Greet renders correctly', 
+
+//expectationShekf
+() => {
+    render(<Greet/>)
+} )
